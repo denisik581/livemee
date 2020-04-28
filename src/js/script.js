7119d864dd7fd886f9d81792515bc24bc4283684
denis <drakonniravgol@gmail.com>
2020-04-28 19:50:54 +0300

    finich pc version

--- src/js/script.js
+++ src/js/script.js
@@ -6,4 +6,39 @@ $(document).ready(function(){
         $(".plus").toggleClass("questionAnswer__wrapper-item_active");
     });
 
+    var positions = [],
+    currentActive = null,
+    links = $('.scroll-to');
+
+    $(".anchor").each(function(){
+        positions.push({
+            top: $(this).position().top - 100,
+            a: links.filter('[href="#'+$(this).attr('id')+'"]')
+        });
+    });
+
+    positions = positions.reverse();
+
+    $(window).on('scroll',function() {
+        var winTop = $(window).scrollTop();
+        for(var i = 0; i < positions.length; i++){
+            if(positions[i].top < winTop){
+                if(currentActive !== i){
+                    currentActive = i;
+                    links.removeClass('active');
+                    positions[i].a.addClass("active");
+                }
+                break;
+            }
+        }
+    });
+
+    $("a[href^='#']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+    
+    new WOW().init();
+
 });
